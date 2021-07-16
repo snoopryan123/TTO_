08627280634720cc0da662211df5da0a8c360f84
Ryan Brill <ryguy123@berkeley.edu>
2021-07-15 21:26:46 -0700

    working on shrinkage estimator

--- data_wrangling/data_wrangling_4.R
+++ data_wrangling/data_wrangling_4.R
@@ -1,8 +1,8 @@
 #install.packages("retrosheet")
-library(retrosheet)
+#library(retrosheet)
 library(tidyverse)
-library(pkgcond)
-library(stringr)
+#library(pkgcond)
+#library(stringr)
 
 # -> add additional FEATURES to Dataset_3 to create Dataset_4
 ########################################################################
@@ -14,6 +14,7 @@ library(stringr)
 # INDIVIDUAL BATTER'S QUALITY
   # WOBA_CUMU_BAT === cumulative woba prior to this plate appearance for a given batter during a given season
   # shrinkage estimator to predict end-of-season wOBA, to put everything on same scale ???
+  # JS estimator relies on batting avg H/N to be approx. normal, since H is binomial. wOBA doesn't have this. so can we use JS?
 # INDIVIDUAL PITCHER'S QUALITY
   # WOBA_CUMU_PIT === cumulative woba prior to this plate appearance for a given pitcher during a given season
   # shrinkage estimator to predict end-of-season wOBA, to put everything on same scale ???
@@ -39,8 +40,44 @@ D <- D %>% group_by(YEAR, BAT_ID) %>%
       mutate(cumu.woba.sum = cumsum(replace_na(EVENT_WOBA, 0)),
              cumu.pa.sum = cumsum(replace_na(PA_IND, 0)),
              WOBA_CUMU_BAT = cumu.woba.sum/cumu.pa.sum) %>% # is it plate appearance, or something else?
-      select(!c(cumu.woba.sum, cumu.pa.sum)) %>%
+      #select(!c(cumu.woba.sum, cumu.pa.sum)) %>%
       ungroup()
+
+
+# SHRINKAGE ESTIMATOR: JAMES STEIN ESTIMATOR
+js <- function(X1, sig.sq1) {
+    mu1_hat = sum(X1/sig.sq1) / sum(1/sig.sq1)
+    P1 = length(X1)
+    t.denom = sum( (X1-mu1_hat)^2 / sig.sq1 )
+    mu1_hat + max(1 - (P1-3)/t.denom, 0)*(X1 - mu1_hat)
+}
+
+D <- D %>% arrange(DATE)
+dates_in_order = unique(D %>% arrange(DATE) %>% select(DATE,YEAR))
+
+
+
+# for (k in 1:length(dates_in_order)) {
+#   print(k)
+#   
+#   date = dates_in_order[k,]$DATE
+#   year = dates_in_order[k,]$YEAR
+#   
+#   DD <- D %>% filter(YEAR == year, DATE <= date)
+#   
+#   E <- DD %>% group_by(BAT_ID) %>%
+#               mutate(X1 = asin(sqrt( (cumu.woba.sum+0.25)/(cumu.pa.sum+0.5) )),
+#                      sig.sq1 = 0.25/cumu.pa.sum ) %>%
+#               ungroup()
+#   
+#   
+#   if (k >= 2) browser()
+# }
+
+
+
+
+
 
 
 
@@ -51,7 +88,7 @@ D <- D %>% group_by(YEAR, PIT_ID) %>%
   mutate(cumu.woba.sum = cumsum(replace_na(EVENT_WOBA, 0)),
          cumu.pa.sum = cumsum(replace_na(PA_IND, 0)),
          WOBA_CUMU_PIT = cumu.woba.sum/cumu.pa.sum) %>% # is it plate appearance, or something else?
-  select(!c(cumu.woba.sum, cumu.pa.sum)) %>%
+  #select(!c(cumu.woba.sum, cumu.pa.sum)) %>%
   ungroup()
 
 
@@ -91,6 +128,18 @@ D <- D %>% group_by(YEAR, PIT_ID) %>%
     # CHECK WOBA_CUMU_BAT
     View(D %>% filter(BAT_ID == "abreb001", YEAR == 2011) %>% 
            select(BAT_ID, YEAR, EVENT_WOBA, PA_IND, WOBA_CUMU_BAT))
+  
+    # https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2011&month=0&season1=2011&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=2011-01-01&enddate=2011-12-31&sort=16,d
+    View(tail(D %>% filter(BAT_NAME == "Jose Bautista", YEAR == 2011) %>% 
+           select(BAT_ID, YEAR, EVENT_WOBA, PA_IND, WOBA_CUMU_BAT)))
+    
+    View(tail(D %>% filter(BAT_NAME == "Miguel Cabrera", YEAR == 2011) %>% 
+                select(BAT_ID, YEAR, EVENT_WOBA, PA_IND, WOBA_CUMU_BAT)))
+    
+    View(tail(D %>% filter(BAT_NAME == "Michael Young", YEAR == 2011) %>% 
+                select(BAT_ID, YEAR, EVENT_WOBA, PA_IND, WOBA_CUMU_BAT)))
+    
+    ### VERY CLOSE, but probably a small error in PA, or the denominator in wOBA (perhaps missing -SF)
   
     # CHECK WOBA_CUMU_PIT
     View(D %>% filter(PIT_ID == "weavj003", YEAR == 2010) %>% 
