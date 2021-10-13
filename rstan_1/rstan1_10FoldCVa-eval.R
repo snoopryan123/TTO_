20ae89ed65efed164c02cc0b2169bbb5a247353d
Ryan Brill <ryguy123@berkeley.edu>
2021-10-13 13:51:39 -0400

    try 10 fold

--- rstan_1/rstan1_10FoldCVa-eval.R
+++ rstan_1/rstan1_10FoldCVa-eval.R
@@ -1,10 +1,16 @@
 
 #source("rstan1_10FoldCVa-a.R")
 
+library(tidyverse)
+library(rstan)
+library(loo)
+print()
+
 e1 <- readRDS("job_output/rstan1_10FoldCVa-1.R.rds") 
 e2 <- readRDS("job_output/rstan1_10FoldCVa-2.R.rds") 
 e3 <- readRDS("job_output/rstan1_10FoldCVa-3.R.rds") 
 e4 <- readRDS("job_output/rstan1_10FoldCVa-4.R.rds") 
 e5 <- readRDS("job_output/rstan1_10FoldCVa-5.R.rds") 
 
-loo_compare(e1, e2, e3, e4, e5)
+L = loo_compare(e1, e2, e3, e4, e5)
+L
