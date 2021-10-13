2873678a037dcd7119c0e618a3c743077eda015e
Ryan Brill <ryguy123@berkeley.edu>
2021-10-13 13:56:10 -0400

    try 10 fold

--- rstan_1/rstan1_10FoldCVa-eval.R
+++ rstan_1/rstan1_10FoldCVa-eval.R
@@ -1,5 +1,6 @@
 
 #source("rstan1_10FoldCVa-a.R")
+OUTPUT_FILE = "rstan_1_loo_compare_a1-5"
 
 library(tidyverse)
 library(rstan)
@@ -13,3 +14,5 @@ e5 <- readRDS("job_output/rstan1_10FoldCVa-5.R.rds")
 
 L = loo_compare(e1, e2, e3, e4, e5)
 L
+saveRDS(L, file = paste0("./job_output/", OUTPUT_FILE, ".rds"))
+#e <- readRDS("job_output/rstan_1_loo_compare_a1-5.rds") 
