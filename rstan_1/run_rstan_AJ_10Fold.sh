2627792eac578828db74a93fc0f2bded4522473e
Ryan Brill <ryguy123@berkeley.edu>
2021-10-12 20:26:25 -0400

    try 10 fold

--- rstan_1/run_rstan_AJ_10Fold.sh
+++ rstan_1/run_rstan_AJ_10Fold.sh
@@ -9,7 +9,8 @@
 ## ARRAY JOB
 #$ -t 1-5
 #$ -o job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
-##$ -b y 
-#$ 'R --no-save < rstan1_10FoldCVa-$SGE_TASK_ID.R'
 
-## Rscript --vanilla < rstan1_10FoldCVa-$SGE_TASK_ID.R'
+##$ -b y 
+##$ 'R --no-save < rstan1_10FoldCVa-$SGE_TASK_ID.R'
+
+Rscript --vanilla < rstan1_10FoldCVa-$SGE_TASK_ID.R'
