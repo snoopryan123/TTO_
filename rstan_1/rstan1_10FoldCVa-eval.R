53e1e87aef90575e8b28a05c42d51d0c69c91bc9
Ryan Brill <ryguy123@berkeley.edu>
2021-10-14 16:54:34 -0400

    try 10 fold

--- rstan_1/rstan1_10FoldCVa-eval.R
+++ rstan_1/rstan1_10FoldCVa-eval.R
@@ -1,3 +1,6 @@
+# https://avehtari.github.io/modelselection/CV-FAQ.html#1_Where_to_start
+# https://mc-stan.org/loo/reference/loo-glossary.html
+
 
 #source("rstan1_10FoldCVa-a.R")
 OUTPUT_FILE = "rstan_1_loo_compare_a1-5"
@@ -10,17 +13,25 @@ e1 <- readRDS("job_output/rstan1_10FoldCVa-1.R.rds")
 e2 <- readRDS("job_output/rstan1_10FoldCVa-2.R.rds") 
 e3 <- readRDS("job_output/rstan1_10FoldCVa-3.R.rds") 
 e4 <- readRDS("job_output/rstan1_10FoldCVa-4.R.rds") 
-e5 <- readRDS("job_output/rstan1_10FoldCVa-5.R.rds") 
 
-L = loo_compare(e1, e2, e3, e4, e5)
+L = loo_compare(e1, e2, e3, e4)
 L
 saveRDS(L, file = paste0("./job_output/", OUTPUT_FILE, ".rds"))
 
 
+# If elpd difference (elpd_diff in loo package) is less than 4, the difference is small. 
+# If elpd difference (elpd_diff in loo package) is larger than 4, then 
+# compare that difference to standard error of elpd_diff (provided e.g. by loo package). 
 
-### Interpret
-# https://avehtari.github.io/modelselection/CV-FAQ.html
+# https://avehtari.github.io/modelselection/CV-FAQ.html#11_What_is_the_interpretation_of_ELPD__elpd_loo__elpd_diff
+# https://avehtari.github.io/modelselection/CV-FAQ.html#15_How_to_interpret_in_Standard_error_(SE)_of_elpd_difference_(elpd_diff)
+
+#### Interpret
 # L <- readRDS("job_output/rstan_1_loo_compare_a1-5.rds") 
-library(tidyverse)
-d = abs(L[,1]/L[,2])
-d
+# A <- rbind(unname(L[,1] + 2*L[,2]), unname(L[,1]), unname(L[,1] - 2*L[,2]))
+# colnames(A) <- paste0("model ", 5:1)
+# rownames(A) <- c("elpd_diff + 2*se_diff", "elpd_diff", "elpd_diff - 2*se_diff")
+# A <- round(A,1)
+# A
+
+
