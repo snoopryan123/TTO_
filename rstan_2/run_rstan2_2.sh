8133535670f20bd890389a6eff39e0adc9e2353c
Ryan Brill <ryguy123@berkeley.edu>
2021-10-25 14:56:32 -0400

    remove pit

--- rstan_2/run_rstan2_2.sh
+++ rstan_2/run_rstan2_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -N rstan2_2.R
+#$ -N rstan2_2_removePit.R
 #$ -j y
 #$ -m e -M ryguy123@sas.upenn.edu 
 
@@ -14,4 +14,4 @@
 ## MORE RAM
 #$ -l m_mem_free=10G
 
-Rscript --vanilla rstan2_2.R
+Rscript --vanilla rstan2_2_removePit.R
