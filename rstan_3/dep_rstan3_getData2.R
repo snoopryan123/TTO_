1f95c8c85f4c65122bfd8f739e6bb1b502947d23
Ryan Brill <ryguy123@berkeley.edu>
2021-11-24 09:50:12 -0800

    fixed issue with plotting function in rstan3_overall_

--- rstan_3/dep_rstan3_getData2.R
+++ rstan_3/dep_rstan3_getData2.R
@@ -3,7 +3,6 @@
 ########################################
 
 output_folder = "./job_output/"
-#OUTPUT_FILE = "rstan3_comp.R" #FIXME
 
 library(tidyverse)
 library(rstan)
@@ -12,6 +11,7 @@ library(latex2exp)
 theme_set(theme_bw())
 theme_update(plot.title = element_text(hjust = 0.5))
 if(!interactive()) pdf(NULL)
+NUM_ITERS_IN_CHAIN = 1500 #FIXME #10 
 cores = strtoi(Sys.getenv('OMP_NUM_THREADS')) ### for HPCC
 options(mc.cores = cores) ### for HPCC
 # options(mc.cores = parallel::detectCores()) # use this on my computer
@@ -49,65 +49,32 @@ U <- as.matrix(BATTER_IDX_dummies)
 O <- as.matrix(ORDER_CT_dummies)
 X <- as.matrix(D %>% select(std_WOBA_FINAL_BAT_19, std_WOBA_FINAL_PIT_19, HAND_MATCH, BAT_HOME_IND))
 y <- matrix(D$std_EVENT_WOBA_19, ncol=1)
+D_bsn = cbind(S,X)
+D_ubi = cbind(U,O,X)
 # 10 Fold CV folds
 set.seed(12345) # make sure to have the same folds each time!
 folds <- loo::kfold_split_random(K=10,N=nrow(y))
 
-############################################
-########### BATTER_SEQ_NUM MODEL ###########
-############################################
+##################################
+########### FIT MODELS ###########
+##################################
 
-file_bsn = 'tto3_bsn.stan'
-model_bsn <- stan_model(file = file_bsn, model_name = file_bsn)
+file = 'tto3_both.stan'
+model <- stan_model(file = file, model_name = file)
 
-fit_model_bsn <- function(fold_num) {
+fit_model <- function(fold_num, key) {
   # training data - exclude FOLD_NUM, unless FOLD_NUM is NA 
   train_rows = ifelse(is.na(fold_num), TRUE, which(folds != fold_num))
   #train_rows = which(folds != fold_num)
   y_train = y[train_rows,]
-  X_train = X[train_rows,]
-  S_train = S[train_rows,]
-  data_train <- list(
-    y=y_train,S=S_train,X=X_train,
-    n=nrow(X_train),p_x=ncol(X_train),p_s=ncol(S_train)
-  )
+  D_bsn_train = D_bsn[train_rows,]
+  D_ubi_train = D_ubi[train_rows,]
+  D_train <- if (key=="ubi") D_ubi_train else D_bsn_train
+  data_train <- list(y=y_train, D = D_train,  n=nrow(D_train), p = ncol(D_train))
   # Train the models
   seed = 12345
   set.seed(seed)
-  NUM_ITERS_IN_CHAIN = 1500 #FIXME #10 
-  fit <- sampling(model_bsn,
-                  data = data_train,
-                  iter = NUM_ITERS_IN_CHAIN,
-                  chains = cores, #1 #cores, 
-                  cores = cores, # HPCC
-                  seed = seed)
-  fit
-}
-
-############################################
-########### UNIQUE_BAT_IDX MODEL ###########
-############################################
-
-file_ubi = 'tto3_ubi.stan'
-model_ubi <- stan_model(file = file_ubi, model_name = file_ubi)
-
-fit_model_ubi <- function(fold_num) {
-  # training data - exclude FOLD_NUM, unless FOLD_NUM is NA 
-  train_rows = ifelse(is.na(fold_num), TRUE, which(folds != fold_num))
-  #train_rows = which(folds != fold_num)
-  y_train = y[train_rows,]
-  X_train = X[train_rows,]
-  U_train = U[train_rows,]
-  O_train = O[train_rows,]
-  data_train <- list(
-    y=y_train,X=X_train,U=U_train,O=O_train,
-    n=nrow(X_train),p_x=ncol(X_train),p_u=ncol(U_train),p_o=ncol(O_train)
-  )
-  # Train the models
-  NUM_ITERS_IN_CHAIN = 1500
-  seed = 12345
-  set.seed(seed)
-  fit <- sampling(model_ubi,
+  fit <- sampling(model,
                   data = data_train,
                   iter = NUM_ITERS_IN_CHAIN,
                   chains = cores, #1 #cores, 
@@ -119,6 +86,73 @@ fit_model_ubi <- function(fold_num) {
 ######################################
 ########### PLOT FUNCTIONS ###########
 ######################################
+
+# plot_bsn_ <- function(fitB) {
+#   alpha_fit = summary(fitB)$summary[2:(dim(S)[2]+1),c(4,1,8)]
+#   A = alpha_fit
+#   colnames(A) = c("lower","avg","upper")
+#   A = as_tibble(A[1:27,])
+#   A$bn = 1:27
+#   # PRODUCTION PLOT
+#   theme_update(plot.title = element_text(hjust = 0.5))
+#   production_plot = A %>% 
+#     ggplot(aes(x=bn, y=avg)) +
+#     geom_errorbar(aes(ymin = lower, ymax = upper), fill = "black", width = .4) +
+#     geom_point(color="dodgerblue2", shape=21, size=2, fill="white") + 
+#     geom_vline(aes(xintercept = 9.5), size=1.2) +
+#     geom_vline(aes(xintercept = 18.5), size=1.2) +
+#     #labs(title = "Pitcher Effectiveness") +
+#     labs(title = TeX("Posterior distribution of $\\alpha$")) + 
+#     theme(legend.position="none") +
+#     scale_x_continuous(name=TeX("Batter sequence number $k$"),
+#                        limits = c(0,28),
+#                        breaks = c(0,5,10,15,20,25)) +
+#     scale_y_continuous(name=TeX("$\\alpha_k$"),
+#                        #limits = c(-.02, .03),
+#                        breaks = seq(-.09, .09, .005)
+#     ) 
+#   production_plot
+# }
+# 
+# plot_ubi_ <- function(fitU) {
+#   beta_fit = summary(fitU)$summary[2:(dim(U)[2]+1),c(4,1,8)]
+#   gamma_fit = summary(fitU)$summary[(dim(U)[2]+2):(dim(U)[2]+dim(O)[2]+1),c(4,1,8)]
+#   B = do.call(rbind, replicate(3, beta_fit[1:9,], simplify=FALSE))
+#   G = rbind(
+#     do.call(rbind, replicate(9, gamma_fit[1,], simplify=FALSE)),
+#     do.call(rbind, replicate(9, gamma_fit[2,], simplify=FALSE)),
+#     do.call(rbind, replicate(9, gamma_fit[3,], simplify=FALSE))
+#   )
+#   A = B+G
+#   rownames(A) = NULL
+#   colnames(A) = c("lower","avg","upper")
+#   A = as_tibble(A)
+#   A$bn = 1:27
+#   # PRODUCTION PLOT
+#   theme_update(plot.title = element_text(hjust = 0.5))
+#   XLABS = c("", paste0("(",1,",",1:9,")"), paste0("(",2,",",1:9,")"), paste0("(",3,",",1:9,")"))
+#   BREAKS = seq(1,28,by=2)#c(1,6,11,16,21,26)#c(0,5,10,15,20,25)
+#   production_plot = A %>% 
+#     ggplot(aes(x=bn, y=avg)) +
+#     geom_errorbar(aes(ymin = lower, ymax = upper), fill = "black", width = .4) +
+#     geom_point(color="dodgerblue2", shape=21, size=2, fill="white") + 
+#     geom_vline(aes(xintercept = 9.5), size=1.2) +
+#     geom_vline(aes(xintercept = 18.5), size=1.2) +
+#     #labs(title = "Pitcher Effectiveness") +
+#     labs(title = TeX("Posterior distribution of $\\beta +\\gamma$")) + 
+#     theme(legend.position="none") +
+#     scale_x_continuous(name=TeX("(order Count $l$, unique batter index $k$)"),
+#                        limits = c(0,28),
+#                        breaks = BREAKS,
+#                        labels =  XLABS[BREAKS+1]) +
+#     scale_y_continuous(name=TeX("$\\beta_{k} + \\gamma_{l}$"),
+#                        #limits = c(-.02, .03),
+#                        #breaks = seq(-.09, .09, .005)
+#     ) 
+#   production_plot
+# }
+
+
 
 # RESCALE the coefficients back to un-standardized form
 #mu_y = mean(D$EVENT_WOBA_19)
@@ -138,6 +172,8 @@ plot_bsn0 <- function(fit) {
   
   # due to autocorrelation, keep every other posterior sample
   #draws <- draws[seq(1,nrow(draws),2),]
+  
+  
   
   # compute mean and 2.5%, 97.5% quantiles of posterior samples
   p = 27 #dim(BATTER_SEQ_dummies)[2]
@@ -233,7 +269,7 @@ plot_ubi0 <- function(fit) {
   
   XLABS = c("", paste0("(",1,",",1:9,")"), paste0("(",2,",",1:9,")"), paste0("(",3,",",1:9,")"))
   BREAKS = seq(1,28,by=2)#c(1,6,11,16,21,26)#c(0,5,10,15,20,25)
-  
+
   # PRODUCTION PLOT
   theme_update(plot.title = element_text(hjust = 0.5))
   production_plot = A4 %>% 
--- rstan_3/rstan3_overall_bsn1.R
+++ rstan_3/rstan3_overall_bsn1.R
@@ -6,6 +6,6 @@ fit = fit_model_bsn(NA)
 saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
 #fit <- readRDS("./job_output/fit_rstan3_overall_bsn1.R.rds") 
 
-p = plot_bsn_(fit)
+p = plot_bsn0(fit)
 p
 ggsave(paste0("./job_output/plot_",OUTPUT_FILE,".png"), p)
