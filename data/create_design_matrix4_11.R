bd4ba7c83c0a18df3c7ef2fd3d4a9edadc944b99
Ryan Brill <ryguy123@berkeley.edu>
2021-12-04 17:14:25 -0500

    HALLELUJAH

--- data/create_design_matrix4_11.R
+++ data/create_design_matrix4_11.R
@@ -37,11 +37,12 @@ E0 <- read_csv(input_filename)
 ########### PITCHER QUALITY FUNCTION ###########
 ################################################
 
-get_PQ <- function(E00, sigma_scale=1) {
+get_PQ <- function(G0, sigma_scale=1) {
+    
     ########### theta_bar_0 for pitchers ########### 
     {
       # prev_szn_avg_woba === pitcher's end-of-season avg. wOBA from the previous season
-      pit_szns = E00 %>% group_by(YEAR, PIT_ID) %>%
+      pit_szns = G0 %>% group_by(YEAR, PIT_ID) %>%
         summarise(final_szn_woba = unique(WOBA_FINAL_PIT_19)) %>% 
         arrange(PIT_ID,YEAR) %>%
         ungroup() %>%
@@ -105,7 +106,7 @@ get_PQ <- function(E00, sigma_scale=1) {
     ########### tau for pitchers ########### 
     {
       # avg. wOBA for each pitcher-game
-      pit_games = E00 %>% group_by(YEAR,GAME_ID,PIT_ID,DATE) %>%
+      pit_games = G0 %>% group_by(YEAR,GAME_ID,PIT_ID,DATE) %>%
         summarise(game_avg_woba = mean(EVENT_WOBA_19)) %>%
         ungroup() %>%
         arrange(PIT_ID,YEAR,DATE)
@@ -168,7 +169,7 @@ get_PQ <- function(E00, sigma_scale=1) {
       pit_games43
     }
     
-    R = E00 %>% filter(YEAR >= 2010) %>% left_join(pit_games43) 
+    R = G0 %>% filter(YEAR >= 2010) %>% left_join(pit_games43) 
     return(R)
 }
 
@@ -191,21 +192,26 @@ View(E1a %>% filter(PIT_ID == "cainm001",YEAR==2010) %>% arrange(DATE,row_idx) %
 ##########################################
 
 # get Batter Quality by re-using the Pitcher Quality function
-E0b = E00 %>% select(row_idx,YEAR,GAME_ID,DATE,BAT_ID,
-                     NUM_WOBA_APP_BAT,NUM_WOBA_APP_FINAL_BAT,
-                     WOBA_FINAL_BAT_19,WOBA_AVG_BAT_19,
+E0b = E00 %>% select(row_idx,YEAR,GAME_ID,DATE,
+                     BAT_ID, WOBA_FINAL_BAT_19, WOBA_AVG_BAT_19,
+                     #NUM_WOBA_APP_BAT,NUM_WOBA_APP_FINAL_BAT,
                      WOBA_APP,EVENT_WOBA_19) 
-E1b = E0b %>% 
-  rename(PIT_ID = BAT_ID, 
-         WOBA_FINAL_PIT_19 = WOBA_FINAL_BAT_19) %>% 
-  get_PQ() %>% 
+E00b = E0b %>% rename(
+  PIT_ID = BAT_ID, 
+  WOBA_FINAL_PIT_19 = WOBA_FINAL_BAT_19,
+  WOBA_AVG_PIT_19 = WOBA_AVG_BAT_19
+)
+E1b = get_PQ(E00b, sigma_scale = 4) %>%
   rename(BQ = PQ,
          BAT_ID = PIT_ID,
-         WOBA_FINAL_BAT_19 = WOBA_FINAL_PIT_19)
-# check
-View(E1b %>% filter(BAT_ID == "mauej001",YEAR==2012) %>% arrange(DATE) %>% 
+         WOBA_FINAL_BAT_19 = WOBA_FINAL_PIT_19,
+         WOBA_AVG_BAT_19 = WOBA_AVG_PIT_19)
+# check "mauej001" "ellsj001" "vottj001" "ryanb002"
+View(E1b %>% filter(BAT_ID == "vottj001",YEAR==2012) %>% arrange(DATE) %>% 
        mutate(cum_avg_woba = cumsum(EVENT_WOBA_19)/cumsum(WOBA_APP)) %>%
-       select(row_idx,YEAR,DATE,GAME_ID,BAT_ID,NUM_WOBA_APP_BAT,NUM_WOBA_APP_FINAL_BAT,WOBA_APP,EVENT_WOBA_19,WOBA_FINAL_BAT_19,cum_avg_woba,WOBA_AVG_BAT_19,BQ))
+       select(row_idx,YEAR,DATE,GAME_ID,BAT_ID,
+              #NUM_WOBA_APP_BAT,NUM_WOBA_APP_FINAL_BAT,
+              WOBA_APP,EVENT_WOBA_19,WOBA_FINAL_BAT_19,cum_avg_woba,WOBA_AVG_BAT_19,BQ))
 
 # new DF with both Pitcher and Batter quality
 X1 = E1a %>% left_join(E1b)
