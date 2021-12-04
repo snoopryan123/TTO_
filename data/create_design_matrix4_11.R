c5c601321ee48e947eb0098206e473fbc2f566ec
Ryan Brill <ryguy123@berkeley.edu>
2021-12-04 16:14:22 -0500

    working on moving avg estimator for pitcher quality

--- data/create_design_matrix4_11.R
+++ data/create_design_matrix4_11.R
@@ -39,66 +39,72 @@ E0 <- read_csv(input_filename)
 
 get_PQ <- function(E00) {
     ########### theta_bar_0 for pitchers ########### 
-    G0 = E00
-    ###CUTOFF_WOBA_APP = 1 #FIXME #50
-  
-    # prev_szn_avg_woba === pitcher's end-of-season avg. wOBA from the previous season
-    pit_szns = G0 %>% group_by(YEAR, PIT_ID) %>%
-      summarise(final_szn_woba = unique(WOBA_FINAL_PIT_19)) %>% 
-      arrange(PIT_ID,YEAR) %>%
-      ungroup() %>%
-      group_by(PIT_ID) %>%
-      mutate(prev_szn_avg_woba = lag(final_szn_woba)) %>%
-      ungroup()
-    pit_szns
+    {
+      G0 = E00
+      ###CUTOFF_WOBA_APP = 1 #FIXME #50
+    
+      # prev_szn_avg_woba === pitcher's end-of-season avg. wOBA from the previous season
+      pit_szns = G0 %>% group_by(YEAR, PIT_ID) %>%
+        summarise(final_szn_woba = unique(WOBA_FINAL_PIT_19)) %>% 
+        arrange(PIT_ID,YEAR) %>%
+        ungroup() %>%
+        group_by(PIT_ID) %>%
+        mutate(prev_szn_avg_woba = lag(final_szn_woba)) %>%
+        ungroup()
+      pit_szns
+        
+      # prev_szn_avg_woba_allPit === for a given season y, the mean wOBA 
+      szns = pit_szns %>% select(-c(final_szn_woba)) %>% drop_na() %>%
+        group_by(YEAR) %>%
+        summarise(prev_szn_avg_woba_allPit = mean(prev_szn_avg_woba)) 
+      szns
       
-    # prev_szn_avg_woba_allPit === for a given season y, the mean wOBA 
-    szns = pit_szns %>% select(-c(final_szn_woba)) %>% drop_na() %>%
-      group_by(YEAR) %>%
-      summarise(prev_szn_avg_woba_allPit = mean(prev_szn_avg_woba)) 
-    szns
-    
-    # insert prev_szn_avg_woba_allPit into pit_szns dataset 
-    pit_szns1 = pit_szns %>% left_join(szns)
-    pit_szns1
-    
-    # theta_bar_0 
-    pit_szns2 = pit_szns1 %>% 
-      mutate(pit_played_in_prev_szn = !is.na(prev_szn_avg_woba),
-             theta_bar_0 = ifelse(pit_played_in_prev_szn, prev_szn_avg_woba, prev_szn_avg_woba_allPit)) 
-    pit_szns2
-    
-    # theta_bar_0 dataframe
-    theta_bar_0_df = pit_szns2 %>% select(YEAR,PIT_ID,theta_bar_0)
-    theta_bar_0_df
-    
+      # insert prev_szn_avg_woba_allPit into pit_szns dataset 
+      pit_szns1 = pit_szns %>% left_join(szns)
+      pit_szns1
+      
+      # theta_bar_0 
+      pit_szns2 = pit_szns1 %>% 
+        mutate(pit_played_in_prev_szn = !is.na(prev_szn_avg_woba),
+               theta_bar_0 = ifelse(pit_played_in_prev_szn, prev_szn_avg_woba, prev_szn_avg_woba_allPit)) 
+      pit_szns2
+      
+      # theta_bar_0 dataframe
+      A1 = pit_szns2 %>% select(YEAR,PIT_ID,theta_bar_0)
+      A1
+    }  
     ########### sigma for pitchers ########### 
     
-    cumsd <- function(x) {
-      TTR::runSD(x, n = 1, cumulative = TRUE)
+    {
+      # cumulative s.d.
+      cumsd <- function(x) {
+        TTR::runSD(x, n = 1, cumulative = TRUE)
+      }
+      
+      # sigma === pitcher-specific szn-by-szn s.d. in avg. wOBA over all his previous seasons
+      pit_szns21 = pit_szns2 %>% select(-c(final_szn_woba,pit_played_in_prev_szn)) %>%
+        group_by(PIT_ID) %>%
+        mutate(has_prev_szns =  row_number() > 1,
+               sigma = ifelse(!has_prev_szns, NA, c(-1,cumsd(prev_szn_avg_woba[2:n()])) )) %>%
+        ungroup()
+      pit_szns21[999:1010,]
+  
+      # sigma === median of the sigma of all pitchers who did pitch in the previous season
+      med_sig = pit_szns21 %>% group_by(YEAR)  %>% drop_na() %>% summarise(med_sig = median(sigma)) 
+      med_sig[med_sig$YEAR < 2010,]$med_sig = NA #want 2010-2019
+      med_sig
+      
+      # fix the NA's of sigma
+      pit_szns22 = pit_szns21 %>% left_join(med_sig) %>%
+        mutate(sigma = ifelse(is.na(sigma), med_sig, sigma)) %>%
+        select(-c(has_prev_szns))
+      pit_szns22[999:1010,]
+      
+      # sigma dataframe
+      sigma_df = pit_szns22 %>% select(YEAR,PIT_ID,theta_bar_0,sigma)
+      sigma_df
     }
-    
-    pit_szns2 = pit_szns1 %>% select(-c(use_prev_szn_avg_woba,prev_year_avg_woba,theta_bar_0)) %>% #select(YEAR,PIT_ID,WOBA_FINAL_PIT_19,woba_prev) %>% 
-      group_by(PIT_ID) %>%
-      mutate(has_2_prev_szns = n() > 2 & row_number() > 2) %>%
-      mutate(sigma = ifelse(has_2_prev_szns, c(-1, cumsd(woba_prev[2:n()])), NA)) %>%
-      ungroup()
-    pit_szns2
-    #View(pit_szns2)  
-    
-    med_sig = pit_szns2 %>% group_by(YEAR)  %>% drop_na() %>% 
-      summarise(med_sig = median(sigma)) 
-    med_sig[med_sig$YEAR <= 2009,]$med_sig = NA #want 2010-2019
-    med_sig
-    
-    pit_szns3 = pit_szns2 %>% left_join(med_sig) %>%
-      mutate(sigma = ifelse(is.na(sigma), med_sig, sigma)) %>%
-      select(-c(med_sig))
-    pit_szns3
-    
-    sigma_df = theta_bar_0_df %>% left_join(pit_szns3 %>% select(YEAR,PIT_ID,sigma))
-    sigma_df
-    
+  
     ########### tau for pitchers ########### 
     
     pit_games = E00 %>% group_by(YEAR,GAME_ID,PIT_ID,DATE) %>%
