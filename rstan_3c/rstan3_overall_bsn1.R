e891e15d87b2c843dd4ce6c9e6ea463aa1b7e7f5
Ryan Brill <ryguy123@berkeley.edu>
2022-01-11 14:36:39 -0800

    3c,3d

---
+++ rstan_3c/rstan3_overall_bsn1.R
@@ -0,0 +1,26 @@
+
+source("rstan3_comp_getData.R")
+
+OUTPUT_FILE = "rstan3_overall_bsn1.R" 
+fit = fit_model_bsn(NA) 
+saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
+#fit <- readRDS("./job_output/fit_rstan3_overall_bsn1.R.rds") 
+
+
+
+
+
+################################
+########### ANALYSIS ###########
+################################
+
+fit_3a <- readRDS("../rstan_3a/job_output/fit_rstan3_overall_bsn1.R.rds")
+fit_3b <- readRDS("./job_output/fit_rstan3_overall_bsn1.R.rds")
+
+p_3a = plot_bsn0(fit_3a)
+p_3b = plot_bsn0(fit_3b)
+p_3a
+p_3b
+ggsave(paste0("./plot_3a_",OUTPUT_FILE,".png"), p_3a)
+ggsave(paste0("./plot_3b_",OUTPUT_FILE,".png"), p_3b)
+
