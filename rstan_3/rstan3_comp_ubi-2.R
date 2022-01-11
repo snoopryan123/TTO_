df667d5022c12921740f01f8ccbb02081e877318
Ryan Brill <ryguy123@berkeley.edu>
2022-01-11 14:37:41 -0800

    3c,3d

--- rstan_3/rstan3_comp_ubi-2.R
+++
@@ -1,9 +0,0 @@
-
-source("rstan3_comp_getData.R")
-
-fold_num = 2 #FIXME
-
-OUTPUT_FILE = paste0("rstan3_comp_ubi-",fold_num,".R") 
-fit = fit_model_ubi(fold_num) 
-saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
-#fit <- readRDS("./job_output/fit_rstan3_comp_ubi-1.R.rds") 
