0c596862ff8e4ba55eb40b677225f9f6fe423608
Ryan Brill <ryguy123@berkeley.edu>
2022-01-19 20:34:43 -0800

    4f-24to29

---
+++ rstan_4f/rstan4f-25.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+output_folder = './job_output/'
+
+### load data
+input_file = "./../data/TTO_dataset_411.csv"  
+D <- read_csv(input_file)
+D <- D %>% drop_na() 
+#FIXME 
+last_third = D$DATE[length(D$DATE) * 0.625]
+D <- D %>% filter(YEAR == 2019) %>% filter(DATE >= last_third) 
+X <- as.matrix(D %>% select(std_WOBA_CURR_BAT_19, std_WOBA_CURR_PIT_19, HAND_MATCH, BAT_HOME_IND)) 
+OUTPUT_FILE = "rstan4f-25.R"
+
+### rstan
+source("rstan5_main.R")
+fit = fit_model_bsn(NA) 
+saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
+
+### plot
+# fit <- readRDS("./job_output/fit_rstan4f-1.R.rds") 
+p = plot_bsn0(fit)
+p
+ggsave(paste0("./plot_",OUTPUT_FILE,".png"), p)
+
--- rstan_4f/run_rstan4f_AJ.sh
+++ rstan_4f/run_rstan4f_AJ.sh
@@ -7,7 +7,7 @@
 #$ -pe openmp 1
 
 ## ARRAY JOB
-#$ -t 21-23
+#$ -t 24-29
 #$ -o job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
 ## MORE RAM
 ####$ -l m_mem_free=10G
