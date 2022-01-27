23c36e172d588fee4613b4d279da5af7ac79db59
Ryan Brill <ryguy123@berkeley.edu>
2022-01-27 13:49:53 -0500

    working on 5_ sim &comp

--- rstan_5/comp/rstan5_comp_analysis.R
+++ rstan_5/comp/rstan5_comp_analysis.R
@@ -86,9 +86,9 @@ for (fold_num in 1:10) {
   df_ubi2 = get_ubi_df(X2, fold_num, ind=30)
   
   test_tib_bsn = bind_rows(test_tib_bsn, df_bsn)
-  test_tib_bsn2 = bind_rows(test_tib_bsn, df_bsn2)
+  test_tib_bsn2 = bind_rows(test_tib_bsn2, df_bsn2)
   test_tib_ubi = bind_rows(test_tib_ubi, df_ubi)
-  test_tib_ubi2 = bind_rows(test_tib_ubi, df_ubi2)
+  test_tib_ubi2 = bind_rows(test_tib_ubi2, df_ubi2)
 }
 
 ########################################
@@ -97,15 +97,6 @@ for (fold_num in 1:10) {
 
 # sd of woba
 sd(D$EVENT_WOBA_19)
-
-# mae (mean absolute error) of posterior predictive mean of y
-mae <- function(d) {
-  sum(abs(d$y - d$ppmean))/nrow(d)
-}
-mae(test_tib_bsn)
-mae(test_tib_ubi)
-mae(test_tib_bsn2)
-mae(test_tib_ubi2)
 
 # rmse of posterior predictive mean of y
 rmse <- function(d) {
@@ -116,6 +107,15 @@ rmse(test_tib_ubi)
 rmse(test_tib_bsn2)
 rmse(test_tib_ubi2)
 
+# mae (mean absolute error) of posterior predictive mean of y
+mae <- function(d) {
+  sum(abs(d$y - d$ppmean))/nrow(d)
+}
+mae(test_tib_bsn)
+mae(test_tib_ubi)
+mae(test_tib_bsn2)
+mae(test_tib_ubi2)
+
 # coverage of posterior predictive intervals of y
 covg <- function(d) {
   sum(d$pplower <= d$y & d$y <= d$ppupper)/nrow(d)
@@ -125,27 +125,16 @@ covg(test_tib_ubi)
 covg(test_tib_bsn2)
 covg(test_tib_ubi2)
 
-# lengths of posterior predictive intervals of y
-lengths_bsn = as_tibble(test_tib_bsn$ppupper - test_tib_bsn$pplower)
-lengths_bsn$model = "model (A)"
-lengths_ubi = as_tibble(test_tib_ubi$ppupper - test_tib_ubi$pplower)
-lengths_ubi$model = "model (B)"
-lengths_bsn2 = as_tibble(test_tib_bsn2$ppupper - test_tib_bsn2$pplower)
-lengths_bsn2$model = "model (C)"
-lengths_ubi2 = as_tibble(test_tib_ubi2$ppupper - test_tib_ubi2$pplower)
-lengths_ubi2$model = "model (D)"
-lengths_df = bind_rows(lengths_bsn,lengths_ubi,lengths_bsn2,lengths_ubi2)
-lppi = lengths_df %>% 
-  ggplot(aes(x=value,fill=model)) +
-  #facet_wrap(~model) +
-  geom_density(alpha=0.2) +
-  xlim(c(1.75,2.05)) +
-  xlab("length") +
-  labs(title="distribution of posterior predictive interval lengths")
-lppi
-
 # ratio of lengths of posterior predictive intervals of y
-length_ratios = lengths_ubi/lengths_bsn
+lengths_bsn = as_tibble(test_tib_bsn$ppupper - test_tib_bsn$pplower)
+lengths_ubi = as_tibble(test_tib_ubi$ppupper - test_tib_ubi$pplower)
+lengths_bsn2 = as_tibble(test_tib_bsn2$ppupper - test_tib_bsn2$pplower)
+lengths_ubi2 = as_tibble(test_tib_ubi2$ppupper - test_tib_ubi2$pplower)
+length_ratiosBA = lengths_ubi/lengths_bsn
+length_ratiosCA = lengths_bsn2/lengths_bsn
+length_ratiosDA = lengths_ubi2/lengths_bsn
+
+
 length_ratios2 = lengths_ubi2/lengths_bsn2
 mean(length_ratios)
 sd(length_ratios)
@@ -163,38 +152,21 @@ lp
 #ggsave("job_output/plot_ppLengthRatio.png", lp)
 
 
-
-
-
-
-
-
-
-##############################################################################
-########### wrong way to get the posterior predictive intervals... ########### 
-##############################################################################
-
-# sigma_fitB = summary(fitB)$summary[1,c(4,1,8)]
-# alpha_fit = summary(fitB)$summary[2:(dim(S)[2]+1),c(4,1,8)]
-# eta_fit = summary(fitB)$summary[(dim(fitB)[3]-4):(dim(fitB)[3]-1),c(4,1,8)]
-# sigma_fitB = summary(fitB)$summary[1,c(4,1,8)]
-# epsilon = c(rnorm(n,0,sd=sigma_fitB[1]),rnorm(n,0,sd=sigma_fitB[2]),rnorm(n,0,sd=sigma_fitB[3]))
-# colnames(alpha_fit) = c("pplower", "ppmean", "ppupper")
-# colnames(eta_fit) = c("pplower", "ppmean", "ppupper")
-# post_predB = S_test%*%alpha_fit + X_test%*%eta_fit + epsilon
-# df_bsn = bind_cols(y_test, as_tibble(post_predB))
-# test_tib_bsn = bind_rows(test_tib_bsn, df_bsn)
-
-
-
-# beta_fit = summary(fitU)$summary[2:(dim(U)[2]+1),c(4,1,8)]
-# gamma_fit = summary(fitU)$summary[(dim(U)[2]+2):(dim(U)[2]+dim(O)[2]+1),c(4,1,8)]
-# delta_fit = summary(fitU)$summary[(dim(fitU)[3]-4):(dim(fitU)[3]-1),c(4,1,8)]
-# sigma_fitU = summary(fitU)$summary[1,c(4,1,8)]
-# epsilonU = c(rnorm(n,0,sd=sigma_fitU[1]),rnorm(n,0,sd=sigma_fitU[2]),rnorm(n,0,sd=sigma_fitU[3]))
-# colnames(beta_fit) = c("pplower", "ppmean", "ppupper")
-# colnames(gamma_fit) = c("pplower", "ppmean", "ppupper")
-# colnames(delta_fit) = c("pplower", "ppmean", "ppupper")
-# post_predU = U_test%*%beta_fit + O_test%*%gamma_fit + X_test%*%delta_fit + epsilonU
-# df_ubi = bind_cols(y_test, as_tibble(post_predU))
-# test_tib_ubi = bind_rows(test_tib_ubi, df_ubi)
+# # lengths of posterior predictive intervals of y
+# lengths_bsn = as_tibble(test_tib_bsn$ppupper - test_tib_bsn$pplower)
+# lengths_bsn$model = "model (A)"
+# lengths_ubi = as_tibble(test_tib_ubi$ppupper - test_tib_ubi$pplower)
+# lengths_ubi$model = "model (B)"
+# lengths_bsn2 = as_tibble(test_tib_bsn2$ppupper - test_tib_bsn2$pplower)
+# lengths_bsn2$model = "model (C)"
+# lengths_ubi2 = as_tibble(test_tib_ubi2$ppupper - test_tib_ubi2$pplower)
+# lengths_ubi2$model = "model (D)"
+# lengths_df = bind_rows(lengths_bsn,lengths_ubi,lengths_bsn2,lengths_ubi2)
+# lppi = lengths_df %>% 
+#   ggplot(aes(x=value,fill=model)) +
+#   #facet_wrap(~model) +
+#   geom_density(alpha=0.2) +
+#   xlim(c(1.75,2.05)) +
+#   xlab("length") +
+#   labs(title="distribution of posterior predictive interval lengths")
+# lppi
