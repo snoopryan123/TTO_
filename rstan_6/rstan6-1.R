bf95d6246b5046d00ad92d8f38c20052e579c7ae
Ryan Brill <ryguy123@berkeley.edu>
2022-02-11 12:31:24 -0500

    enforce 0<BQ<1

--- rstan_6/rstan6-1.R
+++ rstan_6/rstan6-1.R
@@ -6,7 +6,7 @@ input_file = "./../data/TTO_dataset_610.csv"
 D <- read_csv(input_file)
 ##D <- D %>% drop_na() 
 #FIXME 
-D <- D %>% filter(YEAR == 2019) 
+D <- D %>% filter(YEAR == 2019) %>% filter(BQ>0 & BQ<1 & PQ>0 & PQ<1 )
 logit <- function(p) { log(p/(1-p)) }
 X <- as.matrix(D %>% mutate(lBQ=logit(BQ), lPQ=logit(PQ)) %>% select(lBQ, lPQ, HAND_MATCH, BAT_HOME_IND)) 
 OUTPUT_FILE = "rstan6-1.R"
