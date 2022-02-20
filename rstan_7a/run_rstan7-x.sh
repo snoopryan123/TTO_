72839576efe2df3147d9719a96542a30e32a2db0
Ryan Brill <ryguy123@berkeley.edu>
2022-02-20 16:27:03 -0500

    try 7a, 20G ram, 7500 iters, dont include epsilon param

--- rstan_7a/run_rstan7-x.sh
+++ rstan_7a/run_rstan7-x.sh
@@ -4,9 +4,6 @@
 #$ -m e -M ryguy123@sas.upenn.edu 
 
 #$ -o job_output/$JOB_NAME-$JOB_ID.log
-## comment the above, and uncomment the two (2) below for an array job
-##$ -t 1-10
-##$ -o job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
 
 ## this command is to ask for multiple cores for running RStan files: [4]
 #$ -pe openmp 1
