165858879cfbef59900168d9fed6411ece1295f6
Ryan Brill <ryguy123@berkeley.edu>
2022-03-13 15:23:02 -0700

    10 Fold CV Spline

--- rstan_8b/10FoldCV/rstan8_comp_main_spline.R
+++ rstan_8b/10FoldCV/rstan8_comp_main_spline.R
@@ -1,8 +1,8 @@
 source("rstan8_comp_main.R")
 
 ### get FOLD_NUM from other file
-OUTPUT_FILE = paste0("rstan8_comp-",fold_num,".R")
+OUTPUT_FILE = paste0("rstan8_comp_spline-",fold_num,".R")
 fit = fit_model_spline(fold_num) 
 saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
-#fit <- readRDS("./job_output/fit_rstan8_comp-21.R.rds") 
+#fit <- readRDS("./job_output/fit_rstan8_comp_spline-21.R.rds") 
 
