5204c42ff5aaf34d9e624f31de955ad4ccc2cc19
Ryan Brill <ryguy123@berkeley.edu>
2022-08-25 10:27:02 -0700

    model9 simple sim... false positives for TTOP :(

--- model9/sims/run_sim_getFits.sh
+++ model9/sims/run_sim_getFits.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-for i in {1..25}
+for i in {1..1}
 do
    ####echo "moving $i"
-   mv ./job_output/fit_sim_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim_model_bsnBL_$i.rds
-   mv ./job_output/y_sim_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/y_sim_model_bsnBL_$i.rds 
+   mv ./job_output/fit_sim1_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds
+   mv ./job_output/fit_sim2_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds
+   mv ./job_output/y_sim1_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/y_sim1_model_bsnBL_$i.rds 
+   mv ./job_output/y_sim2_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/y_sim2_model_bsnBL_$i.rds 
    boxup
-   mv ../../../Dropbox/HPCC/fit_sim_model_bsnBL_$i.rds   ./job_output/fit_sim_model_bsnBL_$i.rds
-   mv ../../../Dropbox/HPCC/y_sim_model_bsnBL_$i.rds    ./job_output/y_sim_model_bsnBL_$i.rds 
+   mv ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds   ./job_output/fit_sim1_model_bsnBL_$i.rds
+   mv ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds   ./job_output/fit_sim2_model_bsnBL_$i.rds
+   mv ../../../Dropbox/HPCC/y_sim1_model_bsnBL_$i.rds    ./job_output/y_sim1_model_bsnBL_$i.rds 
+   mv ../../../Dropbox/HPCC/y_sim2_model_bsnBL_$i.rds    ./job_output/y_sim2_model_bsnBL_$i.rds 
 done
 
