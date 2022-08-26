c440661c913e0f0548dec96b9182a25016a606b6
Ryan Brill <ryguy123@berkeley.edu>
2022-08-26 07:57:54 -0700

    try 9 more sims.

--- model9/sims/run_sim_10fcv_getFits.sh
+++ model9/sims/run_sim_10fcv_getFits.sh
@@ -2,10 +2,10 @@
 for i in {1..3}
 do
    ####echo "moving $i"
-   mv ./job_output/fit_sim1_model$i_1.rds    ../../../Dropbox/HPCC/fit_sim1_model$i_1.rds
-   mv ./job_output/fit_sim2_model$i_1.rds    ../../../Dropbox/HPCC/fit_sim2_model$i_1.rds
+   mv ./job_output/fit_sim1_model${i}_1.rds    ../../../Dropbox/HPCC/fit_sim1_model${i}_1.rds
+   mv ./job_output/fit_sim2_model${i}_1.rds    ../../../Dropbox/HPCC/fit_sim2_model${i}_1.rds
    boxup
-   mv ../../../Dropbox/HPCC/fit_sim1_model$i_1.rds   ./job_output/fit_sim1_model$i_1.rds
-   mv ../../../Dropbox/HPCC/fit_sim2_model$i_1.rds   ./job_output/fit_sim2_model$i_1.rds
+   mv ../../../Dropbox/HPCC/fit_sim1_model${i}_1.rds   ./job_output/fit_sim1_model${i}_1.rds
+   mv ../../../Dropbox/HPCC/fit_sim2_model${i}_1.rds   ./job_output/fit_sim2_model${i}_1.rds
 done
 
