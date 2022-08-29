996162bff255a954096b6f4b08dcd2b29fa98392
Ryan Brill <ryguy123@berkeley.edu>
2022-08-29 11:40:51 -0700

    get SplOnly fit

--- model9_SplNoBL/run_obs_getFits.sh
+++ model9_SplNoBL/run_obs_getFits.sh
@@ -2,8 +2,8 @@
 for i in {4..7}
 do
    ####echo "moving $i"
-   mv ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds           ../../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds
+   mv ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds           ../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds
    boxup
-   mv ../../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds  ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds    
+   mv ../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds  ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds    
 done
 
