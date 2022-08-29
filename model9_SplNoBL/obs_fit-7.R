c4ba572686c9f9584ed832914a583d0144edf076
Ryan Brill <ryguy123@berkeley.edu>
2022-08-28 21:23:38 -0700

    run spline-only model to see fit

---
+++ model9_SplNoBL/obs_fit-7.R
@@ -0,0 +1,11 @@
+
+YRS = 2018 #2018 #2017:2019
+model = "SingleSplOnly"
+spl_df = 7
+
+OUTPUT_FILE = paste0("obs","_model_", model, 
+                     "yrs_", str_remove_all(paste0(YRS, collapse=''), "20"),
+                     "_df", spl_df, "_") 
+
+source("model9_getData.R") ### get observed data 
+source("obs_fitModel.R") ### fit the model on our observed data
