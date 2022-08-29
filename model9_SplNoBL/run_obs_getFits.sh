f07ad238adc3a4be1aba147a808727ac140d8ee0
Ryan Brill <ryguy123@berkeley.edu>
2022-08-29 11:37:40 -0700

    get SplOnly fit

--- model9_SplNoBL/run_obs_getFits.sh
+++ model9_SplNoBL/run_obs_getFits.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-for i in {1..10}
+for i in {4..7}
 do
    ####echo "moving $i"
-   mv ./job_output/fit_sim1_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds
-   mv ./job_output/fit_sim2_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds
+   mv ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df{$i}_.rds           ../../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df{$i}_.rds
    boxup
-   mv ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds   ./job_output/fit_sim1_model_bsnBL_$i.rds
-   mv ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds   ./job_output/fit_sim2_model_bsnBL_$i.rds
+   mv ../../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df{$i}_.rds  ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df{$i}_.rds    
 done
 
