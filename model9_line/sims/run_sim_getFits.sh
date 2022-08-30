c2c14bfc3c647c71d55ec7c02c1fc16f6be20385
Ryan Brill <ryguy123@berkeley.edu>
2022-08-30 09:50:05 -0700

    fit line model if underlying true is a cubic..

--- model9_line/sims/run_sim_getFits.sh
+++ model9_line/sims/run_sim_getFits.sh
@@ -5,21 +5,21 @@ do
    ## mv ./job_output/fit_sim1_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds
    ## mv ./job_output/fit_sim2_model_bsnBL_$i.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds
 
-   mv ./job_output/fit_sim1_model_bsnBL_$i_underlying_cubic.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i_underlying_cubic.rds
-   mv ./job_output/fit_sim2_model_bsnBL_$i_underlying_cubic.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i_underlying_cubic.rds
+   mv ./job_output/fit_sim1_model_bsnBL_${i}_underlying_cubic.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_${i}_underlying_cubic.rds
+   mv ./job_output/fit_sim2_model_bsnBL_${i}_underlying_cubic.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_${i}_underlying_cubic.rds
 
-   mv ./job_output/fit_sim1_model_bsnBL_$i_underlying_line.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i_underlying_line.rds
-   mv ./job_output/fit_sim2_model_bsnBL_$i_underlying_line.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i_underlying_line.rds
+   mv ./job_output/fit_sim1_model_bsnBL_${i}_underlying_line.rds    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_${i}_underlying_line.rds
+   mv ./job_output/fit_sim2_model_bsnBL_${i}_underlying_line.rds    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_${i}_underlying_line.rds
 
    boxup
 
    ## mv ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i.rds   ./job_output/fit_sim1_model_bsnBL_$i.rds
    ## mv ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i.rds   ./job_output/fit_sim2_model_bsnBL_$i.rds
 
-   mv    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i_underlying_cubic.rds    ./job_output/fit_sim1_model_bsnBL_$i_underlying_cubic.rds 
-   mv    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i_underlying_cubic.rds    ./job_output/fit_sim2_model_bsnBL_$i_underlying_cubic.rds 
+   mv    ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_${i}_underlying_cubic.rds    ./job_output/fit_sim1_model_bsnBL_${i}_underlying_cubic.rds 
+   mv    ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_${i}_underlying_cubic.rds    ./job_output/fit_sim2_model_bsnBL_${i}_underlying_cubic.rds 
   
-   mv     ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_$i_underlying_line.rds    ./job_output/fit_sim1_model_bsnBL_$i_underlying_line.rds
-   mv     ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_$i_underlying_line.rds    ./job_output/fit_sim2_model_bsnBL_$i_underlying_line.rds
+   mv     ../../../Dropbox/HPCC/fit_sim1_model_bsnBL_${i}_underlying_line.rds    ./job_output/fit_sim1_model_bsnBL_${i}_underlying_line.rds
+   mv     ../../../Dropbox/HPCC/fit_sim2_model_bsnBL_${i}_underlying_line.rds    ./job_output/fit_sim2_model_bsnBL_${i}_underlying_line.rds
 done
 
