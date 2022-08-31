154ca38653768bea2d3afd554ae9bc0ee9f632b5
Ryan Brill <ryguy123@berkeley.edu>
2022-08-31 12:24:23 -0700

    fit line model to observed data

--- model9_line/run_obs_getFits.sh
+++ model9_line/run_obs_getFits.sh
@@ -2,8 +2,9 @@
 for i in {12..19}
 do
    ####echo "moving $i"
-   mv ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds           ../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds
+   mv ./job_output/fit_obs_model_lineyrs_${i}_.rds           ../../Dropbox/HPCC/fit_obs_model_lineyrs_${i}_.rds
    boxup
-   mv ../../Dropbox/HPCC/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds  ./job_output/fit_obs_model_SingleSplOnlyyrs_18_df${i}_.rds    
+   mv ../../Dropbox/HPCC/fit_obs_model_lineyrs_${i}_.rds  ./job_output/fit_obs_model_lineyrs_${i}_.rds   
 done
+
 
