bb45ffe7d7575cfe93a001a6cd964aa688d8ed48
Ryan Brill <ryguy123@berkeley.edu>
2022-09-11 14:02:22 -0400

    results on observed data.

---
+++ model9_line_positiveSlope_prior_t/run_results_obs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N run_obs_results
+#$ -j y
+#$ -m e -M ryguy123@sas.upenn.edu 
+
+## this command is to ask for multiple cores for running RStan files: [4]
+#$ -pe openmp 1
+
+#$ -o job_output/$JOB_NAME-$JOB_ID.log
+## MORE RAM
+####$ -l m_mem_free=10G
+
+Rscript --vanilla results_obs.R
