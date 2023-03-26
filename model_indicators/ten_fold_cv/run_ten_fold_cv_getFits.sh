71bc155d662a7897c2f60345bf8fff20d0640e29
Ryan Brill <ryansbrill@gmail.com>
2023-03-26 11:25:48 -0400

    put the logit back

--- model_indicators/ten_fold_cv/run_ten_fold_cv_getFits.sh
+++ model_indicators/ten_fold_cv/run_ten_fold_cv_getFits.sh
@@ -2,8 +2,8 @@
 for i in {1..10}
 do
    #### echo "moving $i"
-   mv ./job_output/fit_ten_fold_cv_${i}_model_spl.rds    ../../../Dropbox/HPCC/fit_ten_fold_cv_${i}_model_spl.rds
+   mv ./job_output/fit_ten_fold_cv_${i}_model_indicators.rds    ../../../.
    boxup
-   mv     ../../../Dropbox/HPCC/fit_ten_fold_cv_${i}_model_spl.rds    ./job_output/fit_ten_fold_cv_${i}_model_spl.rds
+   mv     ../../../Dropbox/HPCC/fit_ten_fold_cv_${i}_model_indicators.rds    ./job_output/.
 done
 
