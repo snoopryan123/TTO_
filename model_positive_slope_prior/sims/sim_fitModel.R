fdbec2f4dfd22225e4d7e7c2b14dfd871cbee333
Ryan Brill <ryansbrill@gmail.com>
2023-05-04 14:49:09 -0700

    try 225 sims

--- model_positive_slope_prior/sims/sim_fitModel.R
+++ model_positive_slope_prior/sims/sim_fitModel.R
@@ -1,7 +1,7 @@
 
-source("../model9_importRstan.R") ### import the RStan model
+source("../A_importRstan.R") ### import the RStan model
 
-fit = fit_model_bsnBL(fold_num=1) ### fit the model
+fit = fit_MODEL(fold_num=1) ### fit the model
 
 saveRDS(fit, file = paste0(output_folder, "fit_", OUTPUT_FILE, ".rds"))
 #fit <- readRDS("./job_output/fit_sim_model_bsnBL_1.rds") 
